4683bf8c5b25a791e7e656353bc0dd97611b8cb1
Kazeheki <11597609+Kazeheki@users.noreply.github.com>
2023-04-10 11:49:34 +0200

    feat: add index page content

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,34 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import JsonBox from '$lib/components/JsonBox.svelte';
+
+  const data = {
+    name: 'Kazeheki',
+    job_title: 'Software&nbsp;Developer',
+    main_skills: ['JavaScript', 'Angular', 'Java (EE)', 'Kubernetes'],
+    main_interests: ['Frontend Development', 'UX', 'Game Development']
+  };
+</script>
+
+<svelte:head>
+  <title>Kazeheki</title>
+</svelte:head>
+
+<JsonBox {data} />
+
+<p class="max-w-prose px-5 md:px-0">
+  Hi! I'm <em>Kazeheki</em>. A Software Developer based in Bavaria, Germany. I'm interested in
+  webpages since I was a teenager when I found a HTML course on
+  <abbr title="www.neopets.com was/is a page where you can care for virtual pets.">Neopets</abbr>.
+  Since then I've learned much more: Java, Angular, Kubernetes, Python and some more. For details
+  check out my <a href="/skills">skills</a> page.
+</p>
+
+<p class="max-w-prose px-5 md:px-0">
+  I'm interested in frontend development but I'm not scared to dive into backend as well. Game
+  development is also very interesting to me, but that is currently a hobby.
+</p>
+
+<p class="max-w-prose px-5 md:px-0">
+  Since 2017 I'm employed as full-stack developer. But I've worked on projects in university some
+  years before already.
+</p>
